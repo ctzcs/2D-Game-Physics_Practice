91ad398d6362e2cff62a5c82a370066fcc4e039b
tw01c <yaohii@foxmail.com>
2024-05-28 22:05:11 +0800

    gx

--- src/Physics/Particle.cpp
+++ src/Physics/Particle.cpp
@@ -1,3 +1,13 @@
 #include "Particle.h"
 
 // TODO:
+Particle::Particle(float x,float y,float mass){
+    this->position = Vec2(x,y);
+    this->mass = mass;
+
+}
+
+Particle::~Particle()
+{
+    
+}
--- src/Physics/Particle.h
+++ src/Physics/Particle.h
@@ -1,6 +1,18 @@
+#include "Vec2.h"
 #ifndef PARTICLE_H
 #define PARTICLE_H
 
 // TODO:
+struct Particle
+{
+    /* data */
+    Vec2 position;
+    Vec2 velocity;
+    Vec2 acceleration;
+    float mass;
+    Particle(float x,float y,float mass);
+    ~Particle();
+
+};
 
 #endif
